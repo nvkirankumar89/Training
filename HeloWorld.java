e55810cc8885184a25a9092922c95eb209e2e9be
Kiran Kumar <kirannaraharisetti@gmail.com>
2020-03-11 16:00:56 -0400

    class name replace to file name

--- HeloWorld.java
+++ HeloWorld.java
@@ -6,6 +6,6 @@ class HeloWorld
 	// Prints "Hello, World" to the terminal window. 
 	public static void main(String args[]) 
 	{ 
-		System.out.println("Hello, World"); 
+		System.out.println("Helo, World"); 
 	} 
 }
